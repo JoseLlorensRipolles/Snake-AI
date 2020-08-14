01b89d270d557e48e9563e57fd072ce69dc1874e
Jose Manuel Llorens <jose.llorens.ripolles@gmail.com>
2020-08-14 17:39:51 +0200

    It moves

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { Point } from '../../model/point';
 import { Enviroment } from '../../model/enviroment';
+
+
+enum KEY_CODE {
+  UP_ARROW = 38,
+  DOWN_ARROW = 40,
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37,
+  W_KEY = 87,
+  S_KEY = 83,
+  A_KEY = 65,
+  D_KEY = 68
+}
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent implements OnInit {
 
   @ViewChild('gameCanvas', { static: true })
   gameCanvas: ElementRef<HTMLCanvasElement>
 
+
+
   ctx: CanvasRenderingContext2D;
   enviroment: Enviroment;
+  direction: string;
 
   constructor() { }
 
@@ -22,7 +39,23 @@ export class GameComponent implements OnInit {
     this.ctx = this.gameCanvas.nativeElement.getContext("2d");
     this.ctx.scale(10, 10);
     this.enviroment = new Enviroment();
+    this.direction = "RIGHT"
 
+    this.drawGame()
+
+    this.gameLoop();
+  }
+
+  gameLoop() {
+    this.enviroment.takeAction(this.direction);
+    this.drawGame();
+    setTimeout(() => {
+      this.gameLoop();
+    }, 500)
+  }
+
+  drawGame() {
+    this.ctx.clearRect(0, 0, this.enviroment.boardDim[0], this.enviroment.boardDim[1]);
     this.drawSnake();
     this.drawApple();
   }
@@ -30,14 +63,49 @@ export class GameComponent implements OnInit {
   drawSnake() {
     this.ctx.fillStyle = "#000000";
     this.enviroment.snake.forEach(element => {
-      this.ctx.fillRect(element.x, element.y, 1, 1)
+      this.ctx.fillRect(
+        element.x,
+        this.enviroment.boardDim[1] - 1 - element.y,
+        1,
+        1)
     })
 
   }
 
-  drawApple(){
+  drawApple() {
     this.ctx.fillStyle = "#FF0000";
-    this.ctx.fillRect(this.enviroment.apple.x, this.enviroment.apple.y, 1, 1);
+    this.ctx.fillRect(
+      this.enviroment.apple.x,
+      this.enviroment.boardDim[1] - 1 - this.enviroment.apple.y,
+      1,
+      1);
   }
 
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    switch (event.keyCode) {
+
+      case KEY_CODE.LEFT_ARROW:
+      case KEY_CODE.A_KEY:
+        this.direction = "LEFT";
+        break;
+
+      case KEY_CODE.RIGHT_ARROW:
+      case KEY_CODE.D_KEY:
+        this.direction = "RIGHT";
+        break;
+
+      case KEY_CODE.UP_ARROW:
+      case KEY_CODE.W_KEY:
+        this.direction = "UP";
+        break;
+
+      case KEY_CODE.DOWN_ARROW:
+      case KEY_CODE.S_KEY:
+        this.direction = "DOWN";
+        break;
+
+    }
+  }
 }
+
--- src/app/model/enviroment.ts
+++ src/app/model/enviroment.ts
@@ -6,8 +6,35 @@ export class Enviroment {
     apple: Point;
 
     constructor(boardSize: number = 10) {
-        this.snake.push(new Point(0, 0));
         this.snake.push(new Point(1, 0));
+        this.snake.push(new Point(0, 0));
         this.apple = new Point(this.boardDim[0] - 1, this.boardDim[1] - 1)
+    }
+
+    takeAction(action: string) {
+
+        let move: [number, number]
+
+        switch (action) {
+            case "UP": {
+                move = [0, 1];
+                break;
+            } case "DOWN": {
+                move = [0, -1];
+                break;
+            } case "RIGHT": {
+                move = [1, 0];
+                break;
+            } case "LEFT":{
+                move = [-1, 0];
+                break;
+            }
+        }
+
+        let tail: Point = this.snake.pop();
+        let head: Point = this.snake[0];
+
+        this.snake.unshift(new Point(head.x + move[0],
+                                     head.y + move[1]));
     }
 }
